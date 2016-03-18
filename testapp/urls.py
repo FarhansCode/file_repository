705c111c55869e4e3b8d8e6ec9c397c250e57c2a
Farhan Khan <khanzf@gmail.com>
2016-03-18 13:19:36 -0400

    Modified template, modified regex regex needs more work...

--- testapp/urls.py
+++ testapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
    url(r'^$', views.index, name='index'),
-   url(r'^repository\/(?P<filedir>[\w|\/|\-|\.]+)*$', views.repository, name='repository'),
+   url(r'^repository\/(?P<filedir>[\w\/\-\.]+)*$', views.repository, name='repository'),
 #   url(r'^file_repository/', include('file_repository.urls', namespace='file_repository')),
 ]
